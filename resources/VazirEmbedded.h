3a39bf675924ad6476f8259e428aceb5d7574f83
DeathAmir <promoly1234@gmail.com>
2026-08-14 17:47:44 +0330

    Remove obsolete Base64 font embedding stub

--- resources/VazirEmbedded.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-// Vazirmatn project: https://github.com/rastikerdar/vazirmatn
-// License: SIL Open Font License 1.1
-// Replace this placeholder with the Base64 payload of the chosen Vazirmatn TTF/WOFF2 file.
-#define IRAUTOX_VAZIR_BASE64 ""
